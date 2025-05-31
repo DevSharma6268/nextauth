ce7c4031af83bc8e1dbf41d20387b49129d10198
dev <devsharma706733@gmail.com>
2025-05-31 22:26:22 +0530

    implement email verification endpoint

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,38 @@
+import {connect} from "@/dbConfig/dbConfig";
+import User from '@/models/userModel';
+import { error } from "console";
+import { verify } from "crypto";
+import { NextRequest,NextResponse} from 'next/server'
+
+
+connect()
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody =await request.json()
+        const {token} =reqBody
+        console.log(reqBody);
+    
+
+      const user =  await User.findOne({verifyToken:token,verifyTokenExpiry:{$gt:Date.now()}})
+
+      if(!user){
+        return NextResponse.json({error:"invalid or expired token "},{status:400})
+      }
+      console.log(user);
+      user.isVerified = true;
+      user.verifyToken =undefined;
+      user.verifyTokenExpiry=undefined;
+
+      const updatedUser =await user.save();
+
+      return NextResponse.json({
+        message:"Email verified successfully",
+        success:true,
+      },{status:200})
+    } catch (error) {
+        return NextResponse.json({error:(error as Error).message},{status:500})
+    }
+}
+
+
